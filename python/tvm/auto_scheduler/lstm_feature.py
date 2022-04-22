864324e08a30ef58c55684f3d2f1d5413d5c4b4d
root <wangshiyu123@hotmail.com>
2022-04-22 19:16:52 +0800

    replace np.empty() for initialization of children vector with np.zeros() during tree feature extraction to avoid random feature values

--- python/tvm/auto_scheduler/lstm_feature.py
+++ python/tvm/auto_scheduler/lstm_feature.py
@@ -30,45 +30,46 @@ def unpack_lstm_feature(byte_arr: bytearray, take_log: Optional[bool] = None, ke
     """Unpack the flatten feature (in byte array format) from c++
     """
     n_tree, offset_child, offset_names, offset_addfea, = \
-        struct.unpack('4i', byte_arr[:4 * 4])
+        struct.unpack('4i', byte_arr[: 4 * 4])
 
     # unpack byte stream
-    child_nums = struct.unpack("%di" % n_tree, byte_arr[offset_child : offset_child + 4 * n_tree])
+    child_nums = struct.unpack("%di" % n_tree, byte_arr[offset_child : (offset_child + 4 * n_tree)])
     children = []
     offset_child += 4 * n_tree
     ct = 0
     for i in range(n_tree):
         num = child_nums[i]
         children.append(struct.unpack("%di" % num,
-                                      byte_arr[offset_child + ct * 4: offset_child + (ct + num) * 4]))
+                                      byte_arr[(offset_child + ct * 4) : (offset_child + (ct + num) * 4)]))
         ct += num
 
-    name_nums = struct.unpack("%di" % n_tree, byte_arr[offset_names:offset_names + 4 * n_tree])
+    name_nums = struct.unpack("%di" % n_tree, byte_arr[offset_names : (offset_names + 4 * n_tree)])
     names = []
     offset_names += 4 * n_tree
     ct = 0
     for i in range(n_tree):
         num = name_nums[i]
         names.append(struct.unpack("%ds" % num,
-                                   byte_arr[offset_names + ct: offset_names + ct + num])[0].decode())
+                                   byte_arr[(offset_names + ct) : (offset_names + ct + num)])[0].decode())
         ct += num
 
-    addfea_nums = struct.unpack("%di" % n_tree, byte_arr[offset_addfea:offset_addfea + 4 * n_tree])
+    addfea_nums = struct.unpack("%di" % n_tree, byte_arr[offset_addfea : (offset_addfea + 4 * n_tree)])
     add_feas = []
     offset_addfea += 4 * n_tree
     ct = 0
     for i in range(n_tree):
         num = addfea_nums[i]
         add_feas.append(struct.unpack("%df" % num,
-                                      byte_arr[offset_addfea + ct * 4: offset_addfea + (ct + num) * 4]))
+                                      byte_arr[(offset_addfea + ct * 4) : (offset_addfea + (ct + num) * 4)]))
         ct += num
 
     # copy children relation to numpy array (with padding)
-    children_np = np.empty((n_tree, MAX_NUM_CHILDREN + 1), dtype=np.int16)
+    # children_np = np.empty((n_tree, MAX_NUM_CHILDREN + 1), dtype=np.int16)
+    children_np = np.zeros((n_tree, MAX_NUM_CHILDREN + 1), dtype=np.int16)
     for i, chi in enumerate(children):
         n_chi = len(chi)
         children_np[i, 0] = n_chi
-        children_np[i, 1:1 + n_chi] = chi
+        children_np[i, 1 : (1 + n_chi)] = chi
 
     # transform string name to integer index
     emb_idx_np = np.empty((n_tree,), dtype=object if keep_name else np.int16)
@@ -80,7 +81,7 @@ def unpack_lstm_feature(byte_arr: bytearray, take_log: Optional[bool] = None, ke
     # copy additional feature to numpy array (with padding)
     add_feas_np = np.zeros((n_tree, MAX_DIM_ADDITIONAL), dtype=np.float32)
     for i, fea in enumerate(add_feas):
-        add_feas_np[i, :addfea_nums[i]] = fea
+        add_feas_np[i, : addfea_nums[i]] = fea
 
     if take_log:
         log_index = add_feas_np > 0
