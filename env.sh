c34e6f62a0d910cc362c5a0a313bb899fa4bdfa7
root <wangshiyu123@hotmail.com>
2022-05-02 00:03:31 +0800

    update env.sh, and add some required packages to install

--- env.sh
+++ env.sh
@@ -1,3 +1,10 @@
-export TVM_HOME=/home/research/tvm
+# Required packages
+# apt-get install -y python3 python3-dev python3-setuptools gcc libtinfo-dev zlib1g-dev build-essential cmake libedit-dev libxml2-dev python3-pip
+# LLVM
+# pip3 install --user numpy decorator attrs scipy tornado psutil xgboost cloudpickle mxnet-cu102
+
+export TVM_HOME=/home/shiyuw3/Research/tvm
 export PYTHONPATH=$TVM_HOME/python:${PYTHONPATH}
-export PATH=/opt/nvidia/nsight-compute/2021.1.0/:/opt/nvidia/nsight-systems/2021.1.3/bin/:$PATH
+export CUDA_HOME=/usr/local/cuda/
+export PATH=/usr/local/cuda-10.2/NVIDIA_Nsight-Compute-2021.1:$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
